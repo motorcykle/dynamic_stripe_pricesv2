6ace5158ebca1803e83f8b36dbeaf6c9a9c20408
motorcykle <mohamedabdulridha1@gmail.com>
2024-01-03 22:52:56 +0100

    button

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -3,25 +3,51 @@ import SubscriptionBtn from "./SubscriptionBtn";
 import { Button } from "./ui/button";
 import { eq } from "drizzle-orm";
 import { db } from "@/lib/db";
-import { auth } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs";
+import { stripe } from "@/lib/stripe";
 
 export default async function Product({ product }: { product: any }) {
     const { userId } = await auth();
+    const user = await currentUser();
+    const return_url = process.env.NEXT_BASE_URL + "/";
 
-    if (!userId) {
-        return false;
-    }
-
-    const _userSubscriptions = await db
+    let _userSubscriptions = userId ? await db
         .select()
         .from(userSubscriptions)
-        .where(eq(userSubscriptions.userId, userId));
+        .where(eq(userSubscriptions?.userId!, userId!)) : []
 
+    async function subscribeToPrice ({priceId}: { priceId: string }) {
+        "use server";
 
-    return <div className=" hover:bg-slate-800">
+        try {
+            const stripeSession = await stripe.checkout.sessions.create({
+                success_url: return_url,
+                cancel_url: return_url,
+                payment_method_types: ["card"],
+                mode: "subscription",
+                billing_address_collection: "auto",
+                customer_email: user?.emailAddresses[0].emailAddress,
+                line_items: [
+                  {
+                    price: priceId,
+                    quantity: 1,
+                  },
+                ],
+                metadata: {
+                  userId,
+                },
+              });
+
+            return stripeSession?.url
+        } catch (error) {
+            console.log(error)
+        } 
+    }
+
+    return <div className=" border rounded-md p-5 mt-5 hover:bg-slate-800">
         <img className=" max-w-28 max-h-28" src={product?.images?.[0]} alt="" />
         <h1>{product?.name}</h1>
         <p>{product?.description}</p>
-        <SubscriptionBtn product={product} currentStatus={_userSubscriptions}/>
+        <SubscriptionBtn subFunction={subscribeToPrice} product={product} currentStatus={_userSubscriptions}/>
     </div>
 }
--- src/components/SubscriptionBtn.tsx
+++ src/components/SubscriptionBtn.tsx
@@ -1,14 +1,28 @@
 "use client"
 
+import { SignInButton, useSession } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 
-export default function SubscriptionBtn ({ currentStatus, product }: { product: any, currentStatus: any }) {
+export default function SubscriptionBtn ({ currentStatus, product, subFunction }: { product: any, currentStatus: any, subFunction: any }) {
+    const session = useSession()
 
     console.log(currentStatus, product)
 
-    const notSubscribed = !currentStatus[0];
+    const notSubscribed = !currentStatus?.[0];
 
-    return (
-        <Button variant={"outline"}>{notSubscribed && "Upgrade to " + product?.name}</Button>
-    )
+    if (!session) return <SignInButton />
+
+    if (notSubscribed) {
+        return (
+            <Button onClick={() => subFunction(product?.default_price)} variant={"outline"}>{"Upgrade to " + product?.name}</Button>
+        )
+    }
+
+    // Subid: {currentStatus.stripeSubscriptionId} Priceid: {currentStatus.stripePriceId}
+
+    if (!notSubscribed) {
+        return (
+            <Button disabled={currentStatus.stripePriceId == product?.default_price}>Sub to this plan</Button>
+        )
+    }
 }
